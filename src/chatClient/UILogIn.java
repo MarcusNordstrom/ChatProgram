dbb983b8f496ed049d0e73645ddb092219347a17
Unknown <abrondin@gmail.com>
2018-03-08 15:00:08 +0100

    a few changes

--- src/chatClient/UILogIn.java
+++ src/chatClient/UILogIn.java
@@ -5,21 +5,31 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.awt.image.ImageFilter;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+import resources.User;
 import resources.UserList;
 
 public class UILogIn extends JPanel implements ActionListener {
@@ -27,9 +37,11 @@ public class UILogIn extends JPanel implements ActionListener {
 	private JLabel lblUsername = new JLabel("Username: ");
 	private JTextField tfUsername = new JTextField();
 	private JButton btnLogIn = new JButton("Log In");
+	private JButton btnUserImage = new JButton("Choose user image");
 	private Client client;
 	private UserList ul = new UserList();
 	JFrame frame;
+	private ImageIcon imageIcon;
 
 
 	public UILogIn(Client client, JFrame frame) {
@@ -38,11 +50,13 @@ public class UILogIn extends JPanel implements ActionListener {
 		setLayout(new BorderLayout());
 		add(panelCenter(), BorderLayout.CENTER);
 		btnLogIn.addActionListener(this);
+		btnUserImage.addActionListener(this);
 	}
 
 	private JPanel panelCenter() {
 		JPanel panel = new JPanel(new GridLayout(3,0));
 		panel.add(panelTop());
+		panel.add(btnUserImage);
 		panel.add(btnLogIn);
 		return panel;
 	}
@@ -66,44 +80,64 @@ public class UILogIn extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == btnLogIn) {
 			//if-sats
-			// om användarnamet finns = connecta & ta upp UIUsers
-			// om användarnamet inte finns = ta bort text i tfUsername.
+			// om anvï¿½ndarnamet finns = connecta & ta upp UIUsers
+			// om anvï¿½ndarnamet inte finns = ta bort text i tfUsername.
 			ul = client.getList();
 			int sameUsers = 0;
-			
+
 			for(int i = 0; i < ul.size(); i++) {
 				if((ul.getUser(i).getName().equals( tfUsername.getText().trim()))) {
 					sameUsers++;
 				}
 			}
 			if(sameUsers == 0) {
-				client.sendUser(tfUsername.getText().trim());
+				client.sendUser(new User(tfUsername.getText().trim(), imageIcon));
 				frame.setVisible(false);
 				System.out.println("UserCreated");
-				
+
 			}else {
 				System.out.println("userExists");
 				tfUsername.setText("");
-				
+
 			}
-			
-	
+
+
+		}
+		if(e.getSource()==btnUserImage) {
+			JFileChooser filechooser = new JFileChooser();
+			int result = filechooser.showOpenDialog(null);
+			filechooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			filechooser.addChoosableFileFilter(new FileNameExtensionFilter("Iamges", "jpg", "png"));
+			filechooser.setAcceptAllFileFilterUsed(true);
+			if(result == JFileChooser.APPROVE_OPTION) {
+				File file = filechooser.getSelectedFile();
+				try {
+					imageIcon = new ImageIcon(ImageIO.read(file));
+					System.out.println(file.toString());
+					imageIcon = new ImageIcon (imageIcon.getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT));
+					btnUserImage.setIcon(imageIcon);
+					btnUserImage.setText("");
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(null, "Wrong file format \nPlease selecet .jpg, .png, .bnp, .gif");
+					//e1.printStackTrace();
+				}
+			}
 		}
 	}
 
-//	public static void main(String[] args) {
-//		try {
-//			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		JFrame frame = new JFrame("Log in");
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-//		frame.setResizable(false);
-//		frame.setPreferredSize(new Dimension(450,250));
-//		frame.add(new UILogIn(new Client()));
-//		frame.pack();
-//		frame.setLocationRelativeTo(null);
-//		frame.setVisible(true);
-//	}
+	//	public static void main(String[] args) {
+	//		try {
+	//			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+	//		} catch (Exception e) {
+	//			e.printStackTrace();
+	//		}
+	//		JFrame frame = new JFrame("Log in");
+	//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	//		frame.setResizable(false);
+	//		frame.setPreferredSize(new Dimension(450,250));
+	//		frame.add(new UILogIn(new Client()));
+	//		frame.pack();
+	//		frame.setLocationRelativeTo(null);
+	//		frame.setVisible(true);
+	//	}
 }
