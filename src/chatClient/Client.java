95cb921fb9e96e8fd51a7aa181c86c9ae643486b
Unknown <sbstn96@gmail.com>
2018-03-09 11:39:04 +0100

    Clientside fixes
    klienten kan nu skicka ett systemmedelande som säger till att klienten dissconnectar.
    systemmedelandet kan även användas som userlist transportsamt borttagit alla oanvända klasser i client

--- src/chatClient/Client.java
+++ src/chatClient/Client.java
@@ -7,64 +7,100 @@ import java.net.Socket;
 
 import javax.swing.ImageIcon;
 
+import resources.SystemMessage;
 import resources.User;
 import resources.UserList;
-
-public class Client{
+import resources.UserMessage;
+/**
+ * 
+ * @author Sebastian
+ *
+ */
+public class Client {
 	private Socket socket;
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
 	private UserList ul = null;
 	private ServerListener sl;
-	
+	private User self;
+	/**
+	 * Constructor
+	 * @param ip
+	 * @param port
+	 * @throws IOException
+	 */
 	public Client(String ip, int port) throws IOException {
 		ul = new UserList();
-		socket = new Socket(ip,port);
+		socket = new Socket(ip, port);
 
 		oos = new ObjectOutputStream(socket.getOutputStream());
 		sl = new ServerListener();
 		sl.start();
 	}
-
-	public void send(String message){//send message to server
-		
-		System.out.println();
+	/**
+	 * Testmessage to server
+	 * @param message
+	 */
+	public void send(String message) {// send Test User to server
+		System.out.println("Sending test message");
 		try {
 			oos.writeObject(new User("kent", new ImageIcon()));
 			oos.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		exit();
-	}		
-
-	public void exit(){		 // ändra
-		if(socket!=null)
+	}
+	/**
+	 * Sends a message to server
+	 * @param message
+	 */
+	public void send(UserMessage message) {
+		System.out.println("Sending user message");
+		try {
+			oos.writeObject(message);
+			oos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Sends a disconnect message to the server
+	 */
+	public void sendDisconnect() {
+		System.out.println("Client disconnected");
+		try {
+			oos.writeObject(new SystemMessage(self, "DISCONNECT"));
+			oos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Close the client/socket and disconnect from the server
+	 */
+	public void exit() { // ändra
+		if (socket != null)
 			try {
+				sendDisconnect();
 				socket.close();
+				System.exit(0);
 			} catch (IOException e) {
 				e.printStackTrace();
-			}		
+			}
 	}
-	
+	/**
+	 * Get for userlist
+	 * @return UserList a list of users
+	 */
 	public UserList getList() {
 		ul.blankList();
-		return ul;		
+		return ul;
 	}
 	
-	public void startListener() {
-		if(sl.interrupted()) {
-			sl = new ServerListener();
-			sl.start();
-		}
-	}
-	
-	public void stopListener() {
-		if(!sl.interrupted()) {
-			sl.interrupt();
-		}
-	}
-	
+	/**
+	 * Sends the newly created user to the server.
+	 * @param user "user created by the loginUI" 
+	 */
 	public void sendUser(User user) {
 		try {
 			oos.writeObject(user);
@@ -72,9 +108,12 @@ public class Client{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		self = user;
 	}
-		
+	/**
+	 * SubClass that listens to server for messages
+	 * @author Sebastian
+	 */
 	private class ServerListener extends Thread {
 		public void run() {
 			Object response;
@@ -84,23 +123,32 @@ public class Client{
 				e1.printStackTrace();
 			}
 			try {
-				while(true) {
+				while (true) {
 					System.out.println("lyssnar");
 					response = ois.readObject();
 					System.out.println(response.toString());
 					System.out.println("mottagit");
-					//input from server
+					if(response instanceof UserMessage) {
+						//Read message and render on ui
+					}else if(response instanceof SystemMessage) {
+						SystemMessage sm = (SystemMessage)response; 
+						if(sm.getPayload() == null) {
+							//Not sure what kind of instruction the server would send to us???
+						}else {
+							if(sm.getPayload() instanceof UserList) {
+								System.out.println("New UserList added");
+								ul = (UserList)(sm.getPayload());
+							}
+						}
+						//Not sure what other messages the server would send
+					}
 				}
-			} catch(IOException e) {
-				
+			} catch (IOException e) {
+
 			} catch (ClassNotFoundException classex) {
-				
+
 			}
-			exit();
 		}
 	}
 
-
-
-	
 }
---
+++ src/resources/SystemMessage.java
@@ -0,0 +1,50 @@
+package resources;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class SystemMessage extends Message implements Serializable{
+
+	private Object payload;
+	private String instruction;
+	
+	
+	/**
+	 * Constructor for sending objects such as the new userlist to clients.
+	 * @param user
+	 * @param recivers
+	 * @param payload
+	 */
+	public SystemMessage(User user, ArrayList<User> recivers, Object payload) {
+		super(user, recivers);
+		this.payload = payload;
+		this.instruction = "UserListUpdate";
+	}
+	/**
+	 * Constructor for sending system messages to the server/ client to tell them what to do.
+	 * Possible commands:
+	 * DISCONNECT //The client is disconnecting
+	 * @param user
+	 * @param recivers
+	 * @param instruction
+	 */
+	public SystemMessage(User user, ArrayList<User> recivers, String instruction) {
+		super(user, recivers);
+		this.payload = null;
+		this.instruction = instruction;
+	}
+	
+	public SystemMessage(User user, String instruction) {
+		super(user, null);
+		this.payload = null;
+		this.instruction = instruction;
+	}
+	
+	public Object getPayload() {
+		return payload;
+	}
+	
+	public String getInstruction() {
+		return instruction;
+	}
+}
